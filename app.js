d0329f196a791a4c4b6215f14c3947af8c3b8999
Azman <azmanbarraquias@gmail.com>
2024-12-17 22:10:09 +0800

    init vue

---
+++ app.js
@@ -0,0 +1,14 @@
+const app = Vue.createApp(
+    {
+        // data, functions
+        // template: '<h2> I amm the template </h2>'
+        data() {
+            return {
+                title: 'Azman hey',
+                num: 1
+            }
+        }
+    }
+)
+
+app.mount('#app')

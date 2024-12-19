6869841de24570364fe13481123b5dd2a489467d
Azman Barraquias <azmanbarraquias@gmail.com>
2024-12-19 12:20:44 +0800

    n

--- vue_js3_reviewer/src/App.vue
+++ vue_js3_reviewer/src/App.vue
@@ -20,7 +20,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #c20f0f;
   margin-top: 60px;
 }
 </style>
